af8a298c6c854358ccd4f8ec556853bb9fe275d5
harshilsapariya <harshilsapariya969@gmail.com>
2023-06-30 04:06:14 +0530

    little change

---
+++ js/data.js
@@ -0,0 +1,8 @@
+let shopItemsData = [
+    {
+      id: "",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-1.jpg",
+    }];
