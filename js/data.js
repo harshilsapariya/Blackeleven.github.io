6e7a216acf491ecf26f33043d2609130c2f8a729
harshilsapariya <133565007+harshilsapariya@users.noreply.github.com>
2023-07-10 02:39:41 +0530

    Delete js directory

--- js/data.js
+++
@@ -1,8 +0,0 @@
-let shopItemsData = [
-    {
-      id: "",
-      name: "Casual Shirt",
-      price: 45,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      img: "images/img-1.jpg",
-    }];
